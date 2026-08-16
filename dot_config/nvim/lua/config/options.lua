ed1965184d2749ef9ce90a646f17784b9718eb11
cyclonoir <amiller.swe@pm.me>
2026-08-16 12:44:21 -0400

    syncing nvim settings with some things from omarchy defaults

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
+-- OSC 52 clipboard forwarding for tmux/SSH sessions. Machine-local concern,
+-- so it lives here rather than in a plugin spec.
+require("config.remote_clipboard").setup()
+
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
